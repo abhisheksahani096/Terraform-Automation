448cc2ac41a42ffb4b78fcae5b50d8714d95f0bf
abhisheksahani096 <abhisheksahani096@gmail.com>
2026-04-26 03:44:46 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state"
+    bucket = "bucket-for-my-project-abhishek"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
